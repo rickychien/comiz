55b2076bf57b0cd7ba10b8f53aab2a9fbf3fa7c2
Ricky Chien <ricky060709@gmail.com>
2016-04-10 01:17:24 +0800

    Fix unable to trigger another episode page

--- src/containers/ComicViewerContainer.jsx
+++ src/containers/ComicViewerContainer.jsx
@@ -12,9 +12,24 @@ class ComicViewerContainer extends React.Component {
     episodeId: PropTypes.number.isRequired
   }
 
+  componentWillReceiveProps(nextProps) {
+    const { dispatch, comicId, episodeId, comics } = nextProps
+    if (!this.getPages(comics, comicId, episodeId) &&
+        (comicId !== this.props.comicId ||
+        episodeId !== this.props.episodeId)) {
+      dispatch(Actions.fetchComicEpisodePagesIfNeeded(comicId, episodeId))
+    }
+  }
+
   componentDidMount() {
     const { dispatch, comicId, episodeId } = this.props
     dispatch(Actions.fetchComicEpisodePagesIfNeeded(comicId, episodeId))
+  }
+
+  getPages = (comics, comicId, episodeId) => {
+    const comic = comics.find(c => c.id === comicId) || {}
+    const episode = comic.episodes.find(e => e.id === episodeId) || {}
+    return episode.pages
   }
 
   onPrevEpisodeClick = () => {
@@ -26,9 +41,10 @@ class ComicViewerContainer extends React.Component {
   }
 
   render() {
+    const { comicId, episodeId, comics } = this.props
     return (
       <ComicViewer
-        pages={ this.props.pages }
+        pages={ this.getPages(comics, comicId, episodeId) }
         prevEpisodeDisabled={ this.props.prevEpisodeDisabled }
         onPrevEpisodeClick={ this.onPrevEpisodeClick }
         nextEpisodeDisabled={ this.props.nextEpisodeDisabled }
@@ -41,14 +57,12 @@ class ComicViewerContainer extends React.Component {
 
 const mapStateToProps = (state) => {
   const { comicId, episodeId } = state.comicViewer
-  const comic = state.comic.comics.find(comic => comic.id === comicId) || {}
-  const episode = comic.episodes.find(episode => episode.id === episodeId) || {}
-  const pages = episode.pages
+  const comics = state.comic.comics
 
   return {
     comicId,
     episodeId,
-    pages
+    comics
   }
 }
 
